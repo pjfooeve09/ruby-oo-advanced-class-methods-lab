3099658dae0389f913e996e24c880bab3bb69bae
Parham Javadi <pjfooeve09@yahoo.com>
2021-01-01 11:13:21 -0800

    commit tree 5df6f6d4f31cd8e3e9bc4ff8c7d73d5f72b17b6d

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,60 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name (name)
+    song = self.new 
+    song.name = name
+    song
+  end
+
+  def self.create_by_name (name)
+    song = self.create     
+    song.name = name
+    song
+  end
+
+  def self.find_by_name (name)
+    self.all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name (name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song|
+    song.name}
+  end
+
+  def self.new_from_filename(filename)
+    new_file = filename.split(" - ") #'initializes a song and artist_name based on the filename format'
+    artist_name = new_file[0]
+    song_name = new_file[1].gsub(".mp3","")
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    new_file = filename.split(" - ") #'initializes and saves a song and artist_name based on the filename format'
+    artist_name = new_file[0]
+    song_name = new_file[1].gsub(".mp3","")
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+
 end
